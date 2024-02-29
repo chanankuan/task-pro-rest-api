2c321bb480e35ff89bfd1b1a8c4822f797cc6218
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-02-29 12:55:11 +0100

    basic confifure cloudinary

--- src/dotenvConfig.js
+++ src/dotenvConfig.js
@@ -6,4 +6,7 @@ export default {
   PORT: process.env.PORT,
   BASE_URL: process.env.BASE_URL,
   SECRET_KEY: process.env.SECRET_KEY,
+  CLOUDINARY_ClOUD_NAME: process.env.CLOUDINARY_ClOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_SECRET_KEY: process.env.CLOUDINARY_SECRET_KEY,
 };
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,10 @@
+import { v2 as cloudinary } from 'cloudinary';
+import dotenvConfig from '../dotenvConfig';
+
+cloudinary.config({
+  cloud_name: dotenvConfig.CLOUDINARY_ClOUD_NAME,
+  api_key: dotenvConfig.CLOUDINARY_API_KEY,
+  api_secret: dotenvConfig.CLOUDINARY_SECRET_KEY,
+});
+
+export { cloudinary };
