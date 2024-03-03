3f5e23d9ab0d628b9942eba3db1ec26296f487fb
AliceBondMe <alice.bondarenko@gmail.com>
2024-03-03 11:16:33 +0200

    edited columns dir

--- src/column/column.model.js
+++ src/column/column.model.js
@@ -2,6 +2,10 @@ import { Schema, model } from 'mongoose';
 
 const columnSchema = new Schema(
   {
+    id: {
+      type: String,
+      required: [true, 'Set FE id for column'],
+    },
     title: {
       type: String,
       required: [true, 'Set title for column'],
@@ -9,6 +13,7 @@ const columnSchema = new Schema(
     board: {
       type: Schema.Types.ObjectId,
       ref: 'Board',
+      required: true,
     },
     owner: {
       type: Schema.Types.ObjectId,
@@ -17,5 +22,21 @@ const columnSchema = new Schema(
   },
   { versionKey: false, timestamps: true }
 );
+
+//needs test
+// columnSchema.pre('remove', async function (next) {
+//   try {
+//     // Access the Column model
+//     const Column = mongoose.model('Column');
+
+//     // Delete all cards associated with this column
+//     await Column.model('Card').deleteMany({ column: this._id });
+
+//     // Continue with the remove operation
+//     return next();
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
 
 export const Column = model('Column', columnSchema);
