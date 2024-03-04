49bb29ec934f845b3eb5c6070f6daefb4d744de9
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-04 22:02:26 +0200

    add register, login, logout user services

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import { User } from '../user/user.model.js';
 import dotenvConfig from '../dotenvConfig.js';
 import { HttpError } from '../helpers/index.js';
@@ -6,16 +7,55 @@ import { HttpError } from '../helpers/index.js';
 const { SECRET_KEY } = dotenvConfig;
 
 const registerUser = async ({ name, email, password }) => {
-  // Create user in db
-  // use save() method
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email is already in use");
+  }
+
+  const newUser = await User.create({ name, email, password });
+
+  const payload = {
+    id: newUser._id
+  }
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(newUser._id, { token });
+
+  newUser.password = undefined;
+  newUser.token = token;
+
+  return newUser;
 };
 
 const loginUser = async ({ email, password }) => {
-  // Sign token and update it in db
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
+
+  const payload = {
+    id: user._id
+  }
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+
+  user.token = token;
+  user.password = undefined;
+
+  return user;
 };
 
 const logoutUser = async userId => {
-  // Remove token from db
+  await User.findByIdAndUpdate(userId, { token: '' });
 };
 
 export default {
