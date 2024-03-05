6d4b19005945ca9a9c21b0843520f04bd23a7f83
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-03-05 22:02:20 +0100

    add backgrounds default filter

--- src/background/background.service.js
+++ src/background/background.service.js
@@ -1,7 +1,9 @@
 import { Background } from './background.model.js';
 
 const getAllMinBackgrounds = async () =>
-  await Background.find().select('backgroundMinURL backgroundMin2xURL');
+  await Background.find({ type: 'default' }).select(
+    'backgroundMinURL backgroundMin2xURL'
+  );
 
 export default {
   getAllMinBackgrounds,
