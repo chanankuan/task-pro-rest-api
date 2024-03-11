3f3fa9e2e87cf29c4d31a41f378daa3735a86a6c
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-03-11 23:45:55 +0100

    Revert "google v2.0+refreshToken"
    This reverts commit 6d5ca8fb6a4c813772651be4cc5ffdbc7adc4097.

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -2,19 +2,10 @@ import authService from './auth.service.js';
 import { trycatch } from '../helpers/index.js';
 import queryString from 'query-string';
 import axios from 'axios';
-import dotenvConfig from '../dotenvConfig.js';
-import { User } from '../user/user.model.js';
-
-const { BASE_URL, FRONTEND_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } =
-  dotenvConfig;
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   const user = await authService.registerUser({ name, email, password });
-  res.cookie('refreshToken', user.refreshToken, {
-    maxAge: 2592000000,
-    httpOnly: true,
-  });
 
   res.status(201).json({ user });
 };
@@ -23,39 +14,24 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   const user = await authService.loginUser({ email, password });
-  res.cookie('refreshToken', user.refreshToken, {
-    maxAge: 2592000000,
-    httpOnly: true,
-  });
 
   res.status(200).json({ user });
 };
 
 const logoutUser = async (req, res) => {
-  const { refreshToken } = req.cookies;
-  await authService.logoutUser(refreshToken);
-  res.clearCookie('refreshToken');
+  const { _id } = req.user;
+
+  await authService.logoutUser(_id);
 
   res.status(200).json({
     message: 'Logout success',
   });
 };
 
-const refresh = async (req, res) => {
-  const { refreshToken } = req.cookies;
-  const user = await authService.refresh(refreshToken);
-  res.cookie('refreshToken', user.refreshToken, {
-    maxAge: 2592000000,
-    httpOnly: true,
-  });
-
-  res.status(200).json({ user });
-};
-
 const googleAuth = async (req, res) => {
   const stringifiedParams = queryString.stringify({
-    client_id: GOOGLE_CLIENT_ID,
-    redirect_uri: `${BASE_URL}/auth/google-redirect`,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: `${process.env.BASE_URL}/auth/google-redirect`,
     scope: [
       'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/userinfo.profile',
@@ -80,9 +56,9 @@ const googleRedirect = async (req, res) => {
     url: `https://oauth2.googleapis.com/token`,
     method: 'post',
     data: {
-      client_id: GOOGLE_CLIENT_ID,
-      client_secret: GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${BASE_URL}/auth/google-redirect`,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: `${process.env.BASE_URL}/auth/google-redirect`,
       grant_type: 'authorization_code',
       code,
     },
@@ -96,34 +72,17 @@ const googleRedirect = async (req, res) => {
     },
   });
 
-  let user = await User.findOne({ email: userData.data.email });
+  // +Логіка
 
-  if (!user) {
-    user = await User.create({
-      name: userData.data.name,
-      email: userData.data.email,
-      password: 'someRandomPassword',
-    });
-  }
-
-  const loginResponse = await authService.loginUser({
-    email: user.email,
-    password: 'someRandomPassword',
-  });
-
-  res.cookie('refreshToken', loginResponse.refreshToken, {
-    maxAge: 2592000000,
-    httpOnly: true,
-  });
-
-  return res.redirect(`${FRONTEND_URL}?email=${userData.data.email}`);
+  return res.redirect(
+    `${process.env.FRONTEND_URL}?email=${userData.data.email}`
+  );
 };
 
 export default {
   registerUser: trycatch(registerUser),
   loginUser: trycatch(loginUser),
   logoutUser: trycatch(logoutUser),
-  refresh: trycatch(refresh),
   googleAuth: trycatch(googleAuth),
   googleRedirect: trycatch(googleRedirect),
 };
